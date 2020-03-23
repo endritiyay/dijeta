14a62cdb26de4e0bfefcfd99296ca3d24d5c8b97
endritiyay <endrithyseni1997@gmail.com>
2020-03-23 23:17:26 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     siteUrl: `https://www.dietabytyqi.com`,
     description: `Artikuj dhe shkrime te ndryshme`,
     hero: {
-      heading: `A Writing Enthusiast`,
+      heading: ``,
       maxWidth: 652,
     },
     social: [
