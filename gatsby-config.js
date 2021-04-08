ccc391fa492d3153f846a72f4e5863ad5186c33b
pixendrit <pixendrit@outlook.com>
2021-04-08 18:30:36 +0200

    Initial commit from gatsby: (https://github.com/endritiyay/dijeta.git)

---
+++ gatsby-config.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+module.exports = {
+  siteMetadata: {
+    title: `Dieta Bytyqi`,
+    name: `Dieta Bytyqi`,
+    siteUrl: `https://www.dietabytyqi.com`,
+    description: `Artikuj dhe shkrime te ndryshme`,
+    hero: {
+      heading: ``,
+      maxWidth: 652,
+    },
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/dietabytyqi`,
+      },
+
+      {
+        name: `instagram`,
+        url: `https://instagram.com/dietabytyqi`,
+      },
+      {
+        name: `linkedin`,
+        url: `https://www.linkedin.com/dietabytyqi`,
+      },
+      {
+        name: `facebook`,
+        url: `https://www.facebook.com/dieta.bytyci`,
+      },
+    ],
+  },
+  plugins: [
+    {
+    resolve: 'gatsby-source-contentful',
+    options: {
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
+  },
+
+     {
+    resolve: '@narative/gatsby-theme-novela',
+    options: {
+      contentPosts: 'content/posts',
+      contentAuthors: 'content/authors',
+      rootPath: '/',
+      basePath: '/',
+      mailchimp: false,
+      sources: {
+        local: false,
+        contentful: true,
+      },
+    },
+  },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Dieta Bytyqi`,
+        short_name: `Dieta`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/assets/eta.png`,
+      },
+    },
+  ],
+};
